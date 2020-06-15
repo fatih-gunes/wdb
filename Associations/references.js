1b924e5bca820b7e7161b33aa72f06c79f521094
fatih gunes <fatih.gunes@yahoo.com>
2020-06-15 21:26:45 +0300

    import

--- Associations/references.js
+++ Associations/references.js
@@ -2,47 +2,29 @@ var mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/blog_demo_2", {useNewUrlParser: true, useUnifiedTopology: true});
 
+var Post = require("./models/post");
 
-// post - title, content
-var postSchema = new mongoose.Schema({
-    title: String,
-    content: String
+var User = require("./models/user");
+
+Post.create({
+    title: "how to cook the best burger pt 4",
+    content: "blah blach dsfdfsdfsdafsad"
+}, function(err, post) {
+    User.findOne({email: "bob@gmail.com"}, function(err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            foundUser.posts.push(post);
+            foundUser.save(function(err, data) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(data);
+                }
+            });
+        }
+    });
 });
-
-var Post = mongoose.model("Post", postSchema);
-
-
-// user - email, name
-var userSchema = new mongoose.Schema({
-    email: String,
-    name: String,
-    posts: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Post"
-    }]
-});
-
-var User = mongoose.model("User", userSchema);
-
-// Post.create({
-//     title: "how to cook the best burger pt 3",
-//     content: "blah blach dsfdfsdfsdafsad"
-// }, function(err, post) {
-//     User.findOne({email: "bob@gmail.com"}, function(err, foundUser) {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             foundUser.posts.push(post);
-//             foundUser.save(function(err, data) {
-//                 if (err) {
-//                     console.log(err);
-//                 } else {
-//                     console.log(data);
-//                 }
-//             });
-//         }
-//     });
-// });
 
 // User.create({
 //     email: "bob@gmail.com",
@@ -51,13 +33,13 @@ var User = mongoose.model("User", userSchema);
 
 // Find user
 // find all posts for that user
-User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, user){
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(user);
-    }
-});
+// User.findOne({email: "bob@gmail.com"}).populate("posts").exec(function(err, user){
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(user);
+//     }
+// });
 
 
 
