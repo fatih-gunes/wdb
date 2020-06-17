43658c964f6aaedf0b41efad7cb1b0f629351d00
fatih gunes <fatih.gunes@yahoo.com>
2020-06-17 21:47:22 +0300

    add

---
+++ YelpCamp/models/campground.js
@@ -0,0 +1,12 @@
+var mongoose = 	mongoose = require("mongoose");
+
+
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	description: String
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
+ 
