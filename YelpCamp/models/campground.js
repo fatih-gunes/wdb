d9a4ad0e02a3b9389b73890bfe095696a6c36e42
fatih gunes <fatih.gunes@yahoo.com>
2020-06-19 19:13:55 +0300

    added comments to campgrounds model

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -1,12 +1,16 @@
-var mongoose = 	mongoose = require("mongoose");
-
-
-// SCHEMA SETUP
+var mongoose = require("mongoose");
+ 
 var campgroundSchema = new mongoose.Schema({
-	name: String,
-	image: String,
-	description: String
+   name: String,
+   image: String,
+   description: String,
+   comments: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Comment"
+      }
+   ]
 });
-
+ 
 module.exports = mongoose.model("Campground", campgroundSchema);
  
